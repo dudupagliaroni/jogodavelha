8c276026e57dc0b47be23b4254ca1fcbdf3a6b7a
Eduardo Pagliaroni <dudupagliaroni@gmail.com>
2021-07-26 19:40:36 -0300

    Update TesteLines.java

--- src/com/eduardo/jogodavelha/TesteLines.java
+++ src/com/eduardo/jogodavelha/TesteLines.java
@@ -4,47 +4,36 @@ import java.lang.reflect.Field;
 
 public class TesteLines {
 
-	static int index = 0;
+	
 
+	static int[] row1 = { 0, 1, 2 };
+	static int[] row2 = { 3, 4, 5 };
+	static int[] row3 = { 6, 7, 8 };
+	static int[] column1 = { 0, 3, 6 };
+	static int[] column2 = { 1, 4, 7 };
+	static int[] column3 = { 2, 5, 8 };
+	static int[] diagonal1 = { 0, 4, 8 };
+	static int[] diagonal2 = { 2, 4, 6 };
+	int[] boardCornersAndCenter = { 0, 2, 4, 6, 8 };
+	static int[] allPositions = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	static int[] allLines[] = { row1, row2, row3, column1, column2, column3, diagonal1, diagonal2 };
+
+	static GameBoard board = new GameBoard();
+	static GameBot bot = new GameBot();
+	int[] lineTest = { 0, 1, 2 };
+	int[] boardTest = { -1, -1, -1, 0, 1, -1, -1, 1, -1 };
+	int[] emptyBoard = { 0, 0, 0, 0, -1, 0, 0, 0, 0 };
+	
+	
+	
 	public static void main(String[] args) {
 
-		int[] row1 = { 0, 1, 2 };
-		int[] row2 = { 3, 4, 5 };
-		int[] row3 = { 6, 7, 8 };
-		int[] column1 = { 0, 3, 6 };
-		int[] column2 = { 1, 4, 7 };
-		int[] column3 = { 2, 5, 8 };
-		int[] diagonal1 = { 0, 4, 8 };
-		int[] diagonal2 = { 2, 4, 6 };
-		int[] boardCornersAndCenter = { 0, 2, 4, 6, 8 };
-		int[] allPositions = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-		int[] allLines[] = { row1, row2, row3, column1, column2, column3, diagonal1, diagonal2 };
 
-		GameBoard board = new GameBoard();
-		GameBot bot = new GameBot();
-		int[] lineTest = { 0, 1, 2 };
-		int[] boardTest = { -1, -1, -1, 0, 1, -1, -1, 1, -1 };
-		int[] emptyBoard = { 0, 0, 0, 0, -1, 0, 0, 0, 0 };
-		board.setBoard(emptyBoard);
-		board.printBoard();
 
 		System.out.println("is started?");
 		int numLine = 1;
 		for (int[] line1 : allLines) {
-			System.out.println("Line " + line1 + " " + bot.checkIfLineStarted(line1, board));
-			Class<?> c = line1.getClass(); 
-			for (Field field : c.getDeclaredFields()) 
-			{ 
-			  try 
-			  { 
-			    System.out.println(field.getName() + "= " +  field.get(c).toString()); 
-			  } 
-			  catch (IllegalAccessException e) 
-			  { 
-			    e.printStackTrace(); 
-			  } 
-			} 
-			numLine++;
+			System.out.println(bot.checkIfLineStarted(line1, board));
 		}
 
 		System.out.println();
@@ -70,6 +59,13 @@ public class TesteLines {
 		System.out.println("método do bot escolher " + bot.botChoosePosition(board));
 		
 		board.printBoard();
+		
+	}
+	
+	public int[] randomBoard() {
+		int[] mark = {-1,1};
+		for (int position : allP)
+	}
 		
 		
 
